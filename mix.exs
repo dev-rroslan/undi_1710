0115911b2b0811ef89df5fa9420d2a61a40f4911
dev-rroslan <user.email>
2024-01-20 16:22:02 +0800

    live view 0.20.3

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Undi.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.20.1"},
+      {:phoenix_live_view, "~> 0.20.3"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.2"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
