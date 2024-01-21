2f3256c52375391d53af8c8a431cbfbac24ab7a6
dev-rroslan <user.email>
2024-01-22 00:11:51 +0800

    update resend mailer

--- config/prod.exs
+++ config/prod.exs
@@ -10,6 +10,10 @@ config :undi, UndiWeb.Endpoint, cache_static_manifest: "priv/static/cache_manife
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Undi.Finch
 
+config :undi, Undi.Mailer, adapter: Resend.Swoosh.Adapter
+
+
+
 # Disable Swoosh Local Memory Storage
 config :swoosh, local: false
 
