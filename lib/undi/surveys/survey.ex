d7ff9727dbd45a61dc3e378c4f6141c21a39f9cd
dev_zain <zainjavaid55@gmail.com>
2024-01-22 12:23:27 +0500

    survey cid format

--- lib/undi/surveys/survey.ex
+++ lib/undi/surveys/survey.ex
@@ -19,15 +19,16 @@ defmodule Undi.Surveys.Survey do
 
   def changeset(survey, attrs) do
     survey
-    |> cast(attrs, [:country_issued_id, :gender, :age, :sokong_fedaral, :sokong_negeri,:datar_padu])
-    |> validate_required([:country_issued_id, :gender, :age, :sokong_fedaral, :sokong_negeri,:datar_padu])
+    |> cast(attrs, [:country_issued_id, :gender, :age, :sokong_fedaral, :sokong_negeri, :datar_padu])
+    |> validate_required([:country_issued_id, :gender, :age, :sokong_fedaral, :sokong_negeri, :datar_padu])
     |> validate_length(:country_issued_id, is: 12)
     |> unique_constraint([:country_issued_id])
     |> validate_format(
          :country_issued_id,
          Regex.compile!(
-           "^([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])([0-6][1-9]|63)([0-9]{4})$"
+           "^([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])(0[1-9]|[1-9][0-9])([0-9]{4})$"
          )
+
        )
   end
 end
