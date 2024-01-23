9ea759172a6c63d238e92486db81306285f648d4
dev-rroslan <user.email>
2024-01-23 19:09:51 +0800

    format

--- lib/undi_web/live/dashboard_live.ex
+++ lib/undi_web/live/dashboard_live.ex
@@ -7,40 +7,28 @@ defmodule UndiWeb.DashboardLive do
   @impl true
   def render(assigns) do
     ~H"""
-        <.header>
-        Survey Dashboard
+    <.header>
+      Survey Dashboard
       <:subtitle>Visualize Realtime Analytics for Surveys</:subtitle>
     </.header>
-           <h5>Total surveys: <%=@total_surveys %></h5>
+    <h5>Total surveys: <%= @total_surveys %></h5>
 
     <p class="text-center text-sm">
-       Age
-      </p>
-    <.line_graph
-    id="line-chart-1"
-    height={300}
-    width={540}
-    dataset={@data_set_for_age}
-    />
+      Age
+    </p>
+    <.line_graph id="line-chart-1" height={300} width={540} dataset={@data_set_for_age} />
 
     <p class="text-center text-sm">
-       Gender
-      </p>
-    <.line_graph
-    id="line-chart-121"
-    height={300}
-    width={540}
-    dataset={@data_set_for_gender}
-    />
-
+      Gender
+    </p>
+    <.line_graph id="line-chart-121" height={300} width={540} dataset={@data_set_for_gender} />
     """
   end
+
   @impl true
   def mount(_params, session, socket) do
-
     {total, data_set_for_age} = Surveys.get_filtered_surveys_by_age()
     data_set_for_gender = Surveys.get_filtered_surveys_by_gender()
-
 
     {
       :ok,
@@ -49,11 +37,6 @@ defmodule UndiWeb.DashboardLive do
       |> assign(:data_set_for_age, data_set_for_age)
       |> assign(:data_set_for_gender, data_set_for_gender)
       |> assign(:total_surveys, total)
-
     }
   end
-
-
-
-
 end
