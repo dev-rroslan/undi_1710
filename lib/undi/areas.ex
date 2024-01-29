a4e2e166b925a1e69959ad31ff3fd0bc285a9a3b
dev-rroslan <user.email>
2024-01-30 06:46:45 +0800

    change area

--- lib/undi/areas.ex
+++ lib/undi/areas.ex
@@ -2,20 +2,10 @@ defmodule Undi.Area do
 
   def areas_list() do
     %{
-      "Lahore" => true,
-      "Karachi" => true,
-      "Peshawar" => true,
-      "Islamabad" => true,
-      "Kuala Lumpur" => true,
-      "Sialkot" => false,
-      "Multan" => false,
-      "Sawat" => false,
-      "Gilgit" => false,
-      "Chakwal" => true,
-      "Nankana Sahib" => true
+      "DUN123" => false,
+      "DUN123village1" => true,
+      "DUN123village2" => true
     }
   end
 
 end
-
-
